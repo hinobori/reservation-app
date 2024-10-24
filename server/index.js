db53c50a89e8f11f849c269109d7bfe40336ede2
hinobori <53688542+hinobori@users.noreply.github.com>
2024-10-25 08:15:15 +0900

    初デプロイ

--- server/index.js
+++ server/index.js
@@ -1,19 +1,32 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('./config/dev')
+const config = require('./config')
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routs/products')
+const path = require('path')
 
 mongoose.connect(config.DB_URI)
   .then(() => {
-    const fakeDb = new FakeDb()
-    fakeDb.initDb()
+    if(process.env.NODE_ENV !== 'production') {
+      const fakeDb = new FakeDb()
+      // fakeDb.initDb()
+    }
+    
   });
 
 const app = express()
 
 app.use('/api/v1/products', productRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist', 'reservation-app')
+  app.get("*", function(req, res) {
+  app.use(express.static(appPath))
+  res.sendFile(path.resolve(appPath, 'index.html'))
+})
+}
+
 
 
 const PORT = process.env.PORT || '3001'
